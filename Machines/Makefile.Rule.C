652b1acfca17e3f1debb2e9da9b509de4c7290ab
Reuben D. Budiardja <reubendb@ornl.gov>
2021-09-20 17:29:44 -0400

    Streamlined C makefiles.

--- Machines/Makefile.Rule.C
+++ Machines/Makefile.Rule.C
@@ -1,9 +1,5 @@
 include ../../Machines/Makefile.inc
 
-jacobi.Fortran : Jacobi.f90
-	$(FC) $(FFLAGS) $(LIBS) $< -o ${@}.${COMPILER}.exe
-	rm -f *.mod
-
 jacobi.C : jacobi.c
 	$(CC) $(FFLAGS) $(LIBS) $< -o ${@}.${COMPILER}.exe
 
